7744faa734463e7244f92165a7e72024061e227a
Murillo Welsi <murillo.welsi@gmail.com>
2023-01-03 23:23:11 +0000

    added migration

---
+++ src/main/resources/db/migration/V1.1__INSERT_INTO_CREATE_USERS_TABLE.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    users(uuid, name, age, updated_at, created_at)
+VALUES
+    ('24bf2487-9e96-40a6-b1f7-dfe5ad91c131', 'Babler', 30, '12/31/2022', '12/31/2022'),
+    ('ec45afff-ca6a-4701-accb-4fc85b1b31dd', 'Murillo', 33, '07/05/2022', '12/31/2022'),
+    ('b9af2ec9-dcc9-41c5-8304-2e0942aec1f2', 'Dunha', 12, '10/01/2022', '12/31/2022');
