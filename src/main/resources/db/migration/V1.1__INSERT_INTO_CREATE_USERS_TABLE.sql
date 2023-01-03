197209ebc3dc907818da0ee7e72997c264d4e792
Murillo Welsi <murillo.welsi@gmail.com>
2023-01-03 23:20:12 +0000

    fixed migrations

--- src/main/resources/db/migration/V1.1__INSERT_INTO_CREATE_USERS_TABLE.sql
+++ src/main/resources/db/migration/V1.1__INSERT_INTO_CREATE_USERS_TABLE.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-    product(uuid, name, age, updated_at, created_at)
+    users(uuid, name, age, updated_at, created_at)
 VALUES
     ('24bf2487-9e96-40a6-b1f7-dfe5ad91c131', 'Babler', 30, '12/31/2022', '12/31/2022'),
     ('ec45afff-ca6a-4701-accb-4fc85b1b31dd', 'Murillo', 33, '07/05/2022', '12/31/2022'),
