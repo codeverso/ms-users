26032b3b26ab775029b0732a856d23fb4f3d5a60
Gabriel Babler <gjacobbabler@atlassian.com>
2023-02-05 17:55:58 -0300

    teste integracao get users

--- src/test/java/com/codeverso/msusers/integration/UserControllerTestIT.java
+++ src/test/java/com/codeverso/msusers/integration/UserControllerTestIT.java
@@ -3,23 +3,20 @@ package com.codeverso.msusers.integration;
 import com.codeverso.msusers.integration.core.IntegrationTest;
 import com.codeverso.msusers.model.entity.UserEntity;
 import com.codeverso.msusers.repository.UserRepository;
-import com.codeverso.msusers.utils.FileUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.UUID;
-
 import static com.codeverso.msusers.integration.factory.UserFactory.createUser;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.jdbc.JdbcTestUtils.deleteFromTables;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @IntegrationTest
 public class UserControllerTestIT {
@@ -34,8 +31,6 @@ public class UserControllerTestIT {
     private JdbcTemplate jdbcTemplate;
 
     private static final String USERS_ENDPOINT = "/users";
-    private static final String USERS_BY_ID_ENDPOINT = "/users/{userId}";
-    private static final Integer UUID_LENGTH = UUID.randomUUID().toString().length();
 
     @BeforeEach
     public void setup() {
@@ -57,209 +52,5 @@ public class UserControllerTestIT {
                 .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[*].name", containsInAnyOrder("userTestOne", "userTestTwo")))
                 .andExpect(jsonPath("$[*].age", containsInAnyOrder(30, 40)));
-    }
-
-    @Test
-    @DisplayName("Should return an user by id")
-    void shouldReturnAnUserById() throws Exception {
-        UserEntity userTestGetUserById = createUser("userTestGetUserById", 15);
-
-        userRepository.save(userTestGetUserById);
-
-        String uuid = userTestGetUserById.getUuid();
-
-        mockMvc.perform(get(USERS_BY_ID_ENDPOINT, uuid))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name", is("userTestGetUserById")))
-                .andExpect(jsonPath("$.age", is(15)))
-                .andExpect(jsonPath("$.uuid", is(uuid)))
-                .andExpect(jsonPath("$.uuid", hasLength(uuid.length())));
-    }
-
-    @Test
-    @DisplayName("Should return 404 NotFound Exception when the user does not exist")
-    void shouldReturn404NotFoundExceptionWhenTheUserDoesNotExist() throws Exception {
-        String uuid = "999";
-
-        mockMvc.perform(get(USERS_BY_ID_ENDPOINT, uuid))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("Should create a new user")
-    void shouldCreateANewUser() throws Exception {
-        String requestBody = FileUtils.getJSONFromFile("createUser.json");
-
-        mockMvc.perform(post(USERS_ENDPOINT)
-                .content(requestBody)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andExpect(header().string("Location", hasLength(UUID_LENGTH)));
-    }
-
-    @Test
-    @DisplayName("Should return 400 when request body does not exist")
-    void shouldReturn400WhenRequestBodyDoesNOtExist() throws Exception {
-        mockMvc.perform(post(USERS_ENDPOINT)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Should update an existing user and return no content")
-    void shouldUpdateAnExistingUserAndReturnNoContent() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("updateUser.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(requestBody))
-                .andExpect(status().isNoContent());
-
-        UserEntity updatedUser = userRepository.findById(uuid).get();
-        assertThat(updatedUser.getUuid(), is(uuid));
-        assertThat(updatedUser.getName(), is("Updated User Integration Test"));
-        assertThat(updatedUser.getAge(), is(70));
-    }
-
-    @Test
-    @DisplayName("Should return bad request when request body is empty")
-    void shouldReturnBadRequestWhenRequestBodyIsEmpty() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                    .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Should return not found when the userId is invalid")
-    void shouldReturnNotFoundWhenTheUserIdIsInvalid() throws Exception {
-        String uuid = "123456";
-
-        String requestBody = FileUtils.getJSONFromFile("updateUser.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("Should return bad request when age key is not present")
-    void shouldReturnBadRequestWhenAgeKeyIsNotPresent() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("updateUserWithoutAge.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Should return bad request when name key is not present")
-    void shouldReturnBadRequestWhenNameKeyIsNotPresent() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("updateUserWithoutName.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Should return bad request when name key is not a string")
-    void shouldReturnBadRequestWhenNameKeyIsNotAString() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("updateUserNameIsNotAString.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
-    }
-
-
-    @Test
-    @DisplayName("Should return bad request when the age is not an Integer")
-    void shouldReturnBadRequestWhenTheAgeIsNotAnInteger() throws Exception {
-        UserEntity userPutTest = createUser("userPutTest", 30);
-        userRepository.save(userPutTest);
-        String uuid = userPutTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("updateUserAgeIsNotAnInteger.json");
-
-        mockMvc.perform(put(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Should partially update an existing user name")
-    void shouldPartiallyUpdateAnExistingUserName() throws Exception {
-        UserEntity userPatchNameTest = createUser("userPatchNameTest", 40);
-        userRepository.save(userPatchNameTest);
-        String uuid = userPatchNameTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("partiallyUpdateUserName.json");
-
-        mockMvc.perform(patch(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isNoContent());
-
-        UserEntity updatedUser = userRepository.findById(uuid).get();
-        assertThat(updatedUser.getUuid(), is(uuid));
-        assertThat(updatedUser.getName(), is("Partially Updated User"));
-        assertThat(updatedUser.getAge(), is(40));
-    }
-
-    @Test
-    @DisplayName("Should partially update an existing user age")
-    void shouldPartiallyUpdateAnExistingUserAge() throws Exception {
-        UserEntity userPatchAgeTest = createUser("userPatchAgeTest", 40);
-        userRepository.save(userPatchAgeTest);
-        String uuid = userPatchAgeTest.getUuid();
-
-        String requestBody = FileUtils.getJSONFromFile("partiallyUpdateUserAge.json");
-
-        mockMvc.perform(patch(USERS_BY_ID_ENDPOINT, uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody))
-                .andExpect(status().isNoContent());
-
-        UserEntity updatedUser = userRepository.findById(uuid).get();
-        assertThat(updatedUser.getUuid(), is(uuid));
-        assertThat(updatedUser.getName(), is("userPatchAgeTest"));
-        assertThat(updatedUser.getAge(), is(55));
-    }
-
-    @Test
-    @DisplayName("Should delete an existing user and return no content")
-    void shouldDeleteAnExistingUserAndReturnNoContent() throws Exception {
-        UserEntity userDeleteTest = createUser("userDeleteTest", 50);
-        userRepository.save(userDeleteTest);
-        String uuid = userDeleteTest.getUuid();
-
-        mockMvc.perform(delete(USERS_BY_ID_ENDPOINT, uuid))
-                .andExpect(status().isNoContent());
-
     }
 }
--- src/test/java/com/codeverso/msusers/service/UserServiceTest.java
+++ src/test/java/com/codeverso/msusers/service/UserServiceTest.java
@@ -1,7 +1,5 @@
 package com.codeverso.msusers.service;
 
-import com.codeverso.msusers.exception.NotFoundException;
-import com.codeverso.msusers.model.dto.UserRequest;
 import com.codeverso.msusers.model.dto.UserResponse;
 import com.codeverso.msusers.model.entity.UserEntity;
 import com.codeverso.msusers.repository.UserRepository;
@@ -14,15 +12,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("User Service Tests")
@@ -56,176 +56,6 @@ public class UserServiceTest {
 
         entities.add(userOne);
         entities.add(userTwo);
-    }
-
-    @Test
-    @DisplayName("Should not delete an user by id because it does not exist")
-    public void shouldNotDeleteAnUserByIdBecauseItDoesNotExist() {
-        String uuid = UUID.randomUUID().toString();
-
-        when(userRepository.findById(uuid))
-                .thenReturn(Optional.empty());
-
-        assertThatCode(() -> userService.deleteUserById(uuid))
-                .doesNotThrowAnyException();
-
-        verify(userRepository, times(1)).findById(uuid);
-        verify(userRepository, times(0)).delete(any(UserEntity.class));
-        verifyNoMoreInteractions(userRepository);
-    }
-    @Test
-    @DisplayName("Should delete an user by id")
-    public void shouldDeleteAnUserById() {
-        String uuid = UUID.randomUUID().toString();
-
-        UserEntity userEntity = entities.get(0);
-
-        when(userRepository.findById(uuid))
-                .thenReturn(Optional.of(userEntity));
-
-        assertThatCode(() -> userService.deleteUserById(uuid))
-                .doesNotThrowAnyException();
-
-        verify(userRepository, times(1)).findById(uuid);
-        verify(userRepository, times(1)).delete(any(UserEntity.class));
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should not partial update an user due to a Not Found Exception")
-    public void shouldNotPartialUpdateAnUserDueToNotFoundException() {
-        UserRequest userRequest = UserRequest.builder()
-                .name("Gabriel")
-                .build();
-
-        String uuid = UUID.randomUUID().toString();
-
-        when(userRepository.findById(uuid))
-                .thenReturn(Optional.empty());
-
-        assertThrows(NotFoundException.class, () -> userService.partialUpdateUser(userRequest, uuid));
-
-        verify(userRepository, times(1)).findById(uuid);
-        verify(userRepository, times(0)).save(any(UserEntity.class));
-
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should partial update an user")
-    public void shouldPartialUpdateAnUser() {
-        UserRequest userRequest = UserRequest.builder()
-                .name("Gabriel")
-                .build();
-
-        UserEntity userEntity = entities.get(0);
-
-        String uuid = UUID.randomUUID().toString();
-
-        when(userRepository.findById(uuid))
-                .thenReturn(Optional.of(userEntity));
-
-        assertThatCode(() -> userService.partialUpdateUser(userRequest, uuid))
-                .doesNotThrowAnyException();
-
-        verify(userRepository, times(1)).findById(uuid);
-        verify(userRepository, times(1)).save(any(UserEntity.class));
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should create a new user")
-    public void shouldCreateANewUser() {
-        UserRequest userRequest = UserRequest.builder()
-                .name("Gabriel")
-                .age(27)
-                .build();
-
-        UserEntity userEntity = entities.get(0);
-
-        given(userRepository.save(any(UserEntity.class)))
-                .willReturn(userEntity);
-
-        String userCreatedUuid = userService.createUser(userRequest);
-
-        assertThat(userCreatedUuid)
-                .isNotNull()
-                .isEqualTo(userEntity.getUuid());
-    }
-
-    @Test
-    @DisplayName("Should throw a not found exception when trying to update an user")
-    public void shouldThrowANotFoundExceptionWhenUpdatingUserById() {
-        UserRequest userRequest = UserRequest.builder()
-                .name("Gabriel")
-                .age(27)
-                .build();
-
-        String invalidUserId = "123";
-
-        when(userRepository.findById(invalidUserId))
-                .thenReturn(Optional.empty());
-
-        assertThrows(NotFoundException.class, () -> userService.updateUser(userRequest, invalidUserId));
-
-        verify(userRepository, times(0)).save(any(UserEntity.class));
-        verify(userRepository, times(1)).findById(invalidUserId);
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should update an user by id")
-    public void shouldUpdateUserById() {
-        UserRequest userRequest = UserRequest.builder()
-                .name("Gabriel")
-                .age(27)
-                .build();
-
-        UserEntity userEntity = entities.get(0);
-        String userId = userEntity.getUuid();
-
-        when(userRepository.findById(userId))
-                .thenReturn(Optional.of(userEntity));
-
-        assertThatCode(() -> userService.updateUser(userRequest, userId))
-                .doesNotThrowAnyException();
-
-        verify(userRepository, times(1)).save(any(UserEntity.class));
-        verify(userRepository, times(1)).findById(userId);
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should throw a NotFoundException due to userId does not exist")
-    public void shouldThrowANotFoundExceptionWhenGetUserById() {
-        String invalidId = "123";
-
-        given(userRepository.findById(invalidId))
-                .willReturn(Optional.empty());
-
-        assertThrows(NotFoundException.class, () -> userService.getUserById(invalidId));
-
-        verify(userRepository, times(1)).findById(invalidId);
-        verifyNoMoreInteractions(userRepository);
-    }
-
-    @Test
-    @DisplayName("Should return an user by id")
-    public void shouldReturnAnUserById() {
-        UserEntity userEntity = entities.get(0);
-
-        given(userRepository.findById(userEntity.getUuid()))
-                .willReturn(Optional.of(userEntity));
-
-        UserResponse userResponse = userService.getUserById(userEntity.getUuid());
-
-        assertNotNull(userResponse);
-        assertEquals(userResponse.getUuid(), userEntity.getUuid());
-        assertEquals(userResponse.getName(), userEntity.getName());
-        assertEquals(userResponse.getAge(), userEntity.getAge());
-
-        verify(userRepository, times(1)).findById(userEntity.getUuid());
-        verifyNoMoreInteractions(userRepository);
     }
 
     @Test
